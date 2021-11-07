b6a0b405ac2eabbcd5d1a6d0952f952808ae7bec
StéphaneDucasse <stephane.ducasse@inria.fr>
2021-11-07 19:25:24 +0100

    cleaning more

--- install/brew-utilities.sh
+++ install/brew-utilities.sh
@@ -22,31 +22,17 @@ binaries=(
   #git-fresh                             # some more git utilities to keep git clean https://github.com/imsky/git-fresh
   #glances                               # https://github.com/nicolargo/glances
   graphicsmagick                        # very powerful image processor
-  heroku/brew/heroku                    # Heroku CLI tool
   hub                                   # GitHub CLI tool
   htop                                  # The best process monitor
-  httpie                                # curl, but better https://github.com/jakubroztocil/httpie
   imagemagick                           # another image processor
   m-cli                                 # macos tool https://github.com/rgcr/m-cli
-  neofetch                              # get info on the running system https://github.com/dylanaraps/neofetch
   ncdu                                  # ncurses disk usage https://dev.yorhel.nl/ncdu
-  #normalize                             # adjust the volume of an audio file https://www.nongnu.org/normalize/
-  #peco                                  # interactive filtering tool https://github.com/peco/peco
-  #pv                                    # monitor progress of data through a pipeline between any two processes https://www.ivarch.com/programs/pv.shtml
-  #pyenv                                 # manage python versions easily https://github.com/pyenv/pyenv
+   #pyenv                                 # manage python versions easily https://github.com/pyenv/pyenv
   #pyenv-virtualenv                      # manage virtualenvs for different python versions https://github.com/pyenv/pyenv-virtualenv
-  #rbenv                                 # manage ruby versions easily https://github.com/rbenv/rbenv
-  #ruby-build                            # dep for rbenv https://github.com/rbenv/ruby-build
-  #getsentry/tools/sentry-cli            # sentry.io cli tool
-  #spaceman-diff                         # diff images from the CLI https://github.com/holman/spaceman-diff
-  #tig                                   # ncurses interface for git https://jonas.github.io/tig/
-  tree                                  # fast recursive directory listing tool http://mama.indstate.edu/users/ice/tree/
+   tree                                  # fast recursive directory listing tool http://mama.indstate.edu/users/ice/tree/
   wget                                  # up-to-date wget
   youtube-dl                            # download a youtube (or other) video by URL or ID https://rg3.github.io/youtube-dl/
 )
-
-brew tap colinstein/imgcat # https://github.com/colinstein/homebrew-imgcat
-brew tap moncho/dry
 
 e_arrow "Installing brew packages"
 
